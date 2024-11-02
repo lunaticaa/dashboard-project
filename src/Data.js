138eea03688915de6414a8f954a69db216e9bcce
amir830047agh@gmail.com <amir830047agh@gmail.com>
2024-11-02 21:15:32 +0330

    chart data

---
+++ src/Data.js
@@ -0,0 +1,50 @@
+export const UserData = [
+  {
+    "name": "far",
+    "User": 3600,
+  },
+  {
+    "name": "ord",
+    "User": 3000,
+  },
+  {
+    "name": "khor",
+    "User": 2000,
+  },
+  {
+    "name": "tir",
+    "User": 2780,
+  },
+  {
+    "name": "mord",
+    "User": 1890,
+  },
+  {
+    "name": "shahr",
+    "User": 2390,
+  },
+  {
+    "name": "mehr",
+    "User": 3150,
+  },
+  {
+    "name": "aban",
+    "User": 1400,
+  },
+  {
+    "name": "azar",
+    "User": 4500,
+  },
+  {
+    "name": "day",
+    "User": 3200,
+  },
+  {
+    "name": "bah",
+    "User": 2050,
+  },
+  {
+    "name": "esf",
+    "User": 1570,
+  }
+]
